489ed81aa0d288cdcc2fb4c2f45be0485320d806
Katariina Parkja <parkja@ut.ee>
2021-11-12 12:19:43 +0200

    added contact information to the contact us page

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import posts from "posts.json";
+import posts from "/posts.json";
 
 Vue.use(Vuex);
 
--- src/views/Contact Us.vue
+++ src/views/Contact Us.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="contact">
-    <h1>This is the contact page</h1>
+    <h1>Contact ManyFuns</h1>
+    <h2>With us you will have the manyest of funs!</h2>
+    <p>Email: many@funs.eu</p>
+    <p>Phone: 696 9420</p>
+    <p>Address: Funs mnt 69, Estonia</p>
   </div>
 </template>
