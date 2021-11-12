af482a543e8db5df26c971fa146bca2771b4c673
Katariina Parkja <parkja@ut.ee>
2021-11-12 12:53:00 +0200

    created footer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-       <h1>ManyFuns application for you</h1>
-       <my-header></my-header>
+    <h1>ManyFuns application for you</h1>
+    <my-header></my-header>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-  export default {
-     components:{
-         'my-header':Header
-     }
-  }
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default {
+  components: {
+    "my-header": Header,
+    "my-footer": Footer,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-footer>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>ManyFuns</strong>
+    </v-col>
+    <v-col>
+      <router-link :to="{ name: 'contact' }">Contact us</router-link> |
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+v-footer {
+  display: flex;
+  justify-content: space-around;
+  background-color: rgb(201, 211, 231);
+  padding: 4px;
+}
+a {
+    font-weight: bold;
+    color: #03090f;
+
+    &.router-link-exact-active {
+      color: #415d81;
+    }
+  }
+</style>
